6edc8f6ddd81fe6790df36b2ce64e4bdbea0c744
kimnamjoon123 <poojasaini152018@gmail.com>
2022-01-15 02:19:46 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,94 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+window.onscroll = () =>{
+
+    searchForm.classList.remove('active');
+  
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+  }  
+
+  window.onload = () =>{
+
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+    fadeOut();
+  
+  }
+
+
+  function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+  }
+  
+  function fadeOut(){
+    setTimeout(loader, 4000);
+  }
+
+ 
+  var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+	if (email.value.length < 9) {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+	if (password.value.length < 6) {
+		password.style.border = "1px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+
+}
+function email_Verify(){
+	if (email.value.length >= 8) {
+		email.style.border = "1px solid silver";
+		email_error.style.display = "none";
+		return true;
+	}
+}
+function pass_Verify(){
+	if (password.value.length >= 5) {
+		password.style.border = "1px solid silver";
+		pass_error.style.display = "none";
+		return true;
+	}
+}
+
+
+
+
+
+    
